b80a53a101de4ba8a0cd6ccf21425aaef1fc6b1d
saileshghimire <saileshghimire1@gmail.com>
2024-07-05 12:27:48 +0545

    schema

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express,{ Express, Request, Response } from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
+
+
+const app = express();
+
+app.use('/api',rootRouter);
+
+
+
+// const PORT = "3000"
+
+app.listen(PORT,()=>{
+    console.log(`server is running at: http://localhost${PORT} `);
+    
+});
---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const authRouter:Router = Router();
+
+authRouter.get('/login',)
+
+export default authRouter;
